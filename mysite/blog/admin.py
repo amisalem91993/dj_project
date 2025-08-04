ba8531f88ea087ef16b0f9ab6aa8749a18156b38
amisalem91993 <amisalem91993@gmail.com>
2025-08-04 17:58:29 +0200

    Voetbalspeler model gemaakt en admin aangepast

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-from .models import Post
-admin.site.register(Post)
+from .models import Voetbalspeler
+
+admin.site.register(Voetbalspeler)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,16 +2,16 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):
+class Voetbalspeler(models.Model):
+    naam = models.CharField(max_length=200)
+    actuele_club = models.CharField(max_length=100)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
+    datum_invoer = models.DateTimeField(default=timezone.now)
+    datum_laatste_aanpassing = models.DateTimeField(blank=True, null=True)
+    
+    def publiceer(self):
+        self.datum_laatste_aanpassing = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.naam
